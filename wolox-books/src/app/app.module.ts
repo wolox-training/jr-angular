691d732e506eae2b539c36ddaaf1bc6524f5feba
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2020-05-04 03:31:12 -0500

    Ordered folder structure as explained in card

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { RegisterComponent } from './register/register.component';
-import { ButtonComponent } from './button/button.component';
+import { AuthComponent } from './screens/Signup/components/auth/auth.component';
+import { RegisterComponent } from './screens/Signup/components/register/register.component';
+import { ButtonComponent } from './components/Button/button.component';
 
 @NgModule({
   declarations: [
