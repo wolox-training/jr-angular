debbff27fcd08874d6d654e70b475bb962cb0923
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2020-05-06 16:36:16 -0500

    applied review comments

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { RegisterComponent } from './register/register.component';
-import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     RegisterComponent,
-    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ wolox-books/src/app/validators/register-validator.ts
@@ -0,0 +1,25 @@
+export class RegisterValidator {
+
+  readonly errorMessages = {
+    required: 'This field is required',
+    email: 'Invalid email format',
+    pattern: 'Must have at least one number and one capital letter',
+    passwordConfirmation: 'The passwords must match'
+  }
+
+  constructor() { }
+
+  passwordConfirmation() {
+    return function(formGroup) {
+      const userPassword = formGroup.controls.password;
+      const confirmedPassword = formGroup.controls.confirmPassword;
+
+      return userPassword.value !== confirmedPassword.value && confirmedPassword.setErrors({ passwordConfirmation: false })
+    }
+  }
+
+  errorMessage(controlName, formGroup) {
+    const controlErrors = formGroup.get(controlName).errors;
+    if (controlErrors) return this.errorMessages[Object.keys(controlErrors).toString()];
+  }
+}
