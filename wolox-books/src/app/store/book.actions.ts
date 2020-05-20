aaa49fce4a5f61fd7292ce7649ccd06720a0b746
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2020-05-20 12:26:07 -0500

    Changed strings that describe action names to be shorter and concise

--- wolox-books/src/app/store/book.actions.ts
+++ wolox-books/src/app/store/book.actions.ts
@@ -2,9 +2,9 @@ import { createAction } from '@ngrx/store';
 import { Book } from 'app/models/book';
 
 export const addBook = createAction(
-  '[Book-item Component] Add a book to the shopping cart', (book: Book) => book
+  '[Book] AddBook', (book: Book) => book
 );
 
 export const removeBook = createAction(
-  '[Shopping-list Component] Remove a book from the shopping cart', (index) => ({ index })
+  '[Book] RemoveBook', (index) => ({ index })
 );
